169574b9de3cced72e784e6b5fa89f3abc6fe818
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-05-30 09:55:02 +0100

    branching Maxime code

--- adk-test-agents/horseragish/agent.py
+++ adk-test-agents/horseragish/agent.py
@@ -5,7 +5,7 @@ from maxlib import process_docs
 # TODO: Move to a config file
 DATA_FOLDER = "../data"
 
-# Maxime version
+# Ricc version - copied from Maxime
 instructions_prompt = """You are a specialized Information Retrieval and Q&A Agent using only this local corpus:
 
 Upon receiving a user's question:
@@ -13,15 +13,13 @@ Upon receiving a user's question:
 2. Use ONLY the local data corpus to answer questions.
 3. For every answer include the source from the local corpus including the local filename
 
-In every response, ensure to use some French words to make it sound more natural and engaging, as if you were a French-speaking person.:
+In every response, ensure to use some Italian words and emojis to make it sound more natural and engaging, as if you were a Italian-speaking person:
 """
 
 
-def get_content_as_text():
-    """TODO(maxime): for function calling its very important to set proper description and TYPE of input and return.
 
-        returns: a string containing the content of the local data corpus. [TODO(maxime): fix this]
-    """
+# Maxime version.
+def get_content_as_text():
     data_path = pathlib.Path(DATA_FOLDER)
     if not data_path.is_absolute():
         data_path = pathlib.Path(__file__).parent.joinpath(DATA_FOLDER)
